8d0f2f448f8a648bf40056b577322feaf82bc3b2
Luiz Carlos da Silva Soares <luizdba@gmail.com>
2019-03-01 12:54:28 -0300

    GraphQL folder

---
+++ GraphQL/src/backend/gql.core/model/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace gql.core.model {
+
+    public class Game {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Company Developer { get; set; }
+        public Company Publisher { get; set; }
+        public DateTime RelaseDate { get; set; }
+    }
+    
+}
